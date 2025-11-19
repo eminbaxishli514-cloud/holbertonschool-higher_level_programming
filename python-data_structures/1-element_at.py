db23cf74d5a51fa308b65a8b4e135e6a1be9149d
eminbaxishli514-cloud <eminbaxishli514@gmail.com>
2025-11-19 09:26:32 +0000

    My new commit

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-	if idx >= len(my_list):
-		return None
-	elif idx < 0:
-		return None
-	else:
-		print(my_list[idx])
+        if idx >= len(my_list):
+                return None
+        elif idx < 0:
+                return None
+        else:
+                print(my_list[idx])
+
+
+
+
+
+
+
