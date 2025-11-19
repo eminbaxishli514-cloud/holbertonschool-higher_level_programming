fb99ee1b9c66ee1f745cf42debc4a8d84536e653
eminbaxishli514-cloud <eminbaxishli514@gmail.com>
2025-11-19 13:14:30 +0000

    My new commit

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     """Returns the key with the highest value in a dictionary."""
+    if a_dictionary is None or len(a_dictionary) == 0:
+        return None
+
     count = 0
     value = 0
     for key in a_dictionary:
