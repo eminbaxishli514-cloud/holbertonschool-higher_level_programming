1ef42eb22d65fdee92fa71faa00a4c6ce128475d
eminbaxishli514-cloud <eminbaxishli514@gmail.com>
2025-11-19 09:14:03 +0000

    My new commit

---
+++ python-import_modules/5-variable_load.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# Import the variable 'a' from variable_load_5.py
+from variable_load_5 import a
+
+# Only run this if the script is executed directly
+if __name__ == "__main__":
+    print(a)
