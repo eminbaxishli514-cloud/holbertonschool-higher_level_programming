19e7717425ad529f1956fd869d728c17f838058b
eminbaxishli514-cloud <eminbaxishli514@gmail.com>
2025-11-19 10:15:13 +0000

    My new commit

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-	if sentence = "":
-		sentence[0] = None
+    """Return the length of a string and its first character (or None if empty)."""
+    if sentence == "":
+        first_char = None
+    else:
+        first_char = sentence[0]
+    return (len(sentence), first_char)
