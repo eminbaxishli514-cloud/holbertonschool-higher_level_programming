d00a0f88aedaf3d23e2e0072566d956ddce78e16
eminbaxishli514-cloud <eminbaxishli514@gmail.com>
2025-11-19 10:34:53 +0000

    M

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -2,7 +2,8 @@
 def delete_at(my_list=[], idx=0):
     """
     Delete the item at a specific position in a list.
-    If idx is out of range or negative, returns the list unchanged.
+
+    If idx is negative or out of range, returns the list unchanged.
     """
     if idx < 0 or idx >= len(my_list):
         return my_list
