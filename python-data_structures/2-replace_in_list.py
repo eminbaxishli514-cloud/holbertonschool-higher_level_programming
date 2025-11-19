f04eaca8952cc887f791bb0aa10661b81fc977ea
eminbaxishli514-cloud <eminbaxishli514@gmail.com>
2025-11-19 09:35:08 +0000

    My new message

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    """Replace an element in a list at a specific index.
-
-    If idx is out of range, do nothing and return None.
-    """
-    if idx < 0 or idx >= len(my_list):
-        return None
-    my_list[idx] = element
+    """Replace an element at a given index in a list."""
+    if 0 <= idx < len(my_list):
+        my_list[idx] = element
     return my_list
