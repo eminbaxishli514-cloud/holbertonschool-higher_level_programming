f6af6f15d0a5328e6e1da9b7499f2f1ed34131ff
eminbaxishli514-cloud <eminbaxishli514@gmail.com>
2025-11-19 09:51:12 +0000

    My new commit

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    """Replace an element at a given index in a list."""
-    if 0 <= idx < len(my_list):
-        my_list[idx] = element
-    return my_list
+    """Replace an element in a list at a specific index without modifying the original list."""
+    new_list = my_list.copy()  # make a copy of the original list
+    if 0 <= idx < len(new_list):
+        new_list[idx] = element
+    return new_list
