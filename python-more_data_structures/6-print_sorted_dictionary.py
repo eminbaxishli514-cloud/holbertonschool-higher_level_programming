b26459638d33f11cf9784efd54ba595f32a434b6
eminbaxishli514-cloud <eminbaxishli514@gmail.com>
2025-11-19 12:53:52 +0000

    My new commit

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-	for i in sorted(a_dictionary):
-		print(f"{i} : {a_dictionary[i]}")
+    """Print a dictionary by ordered keys."""
+    for key in sorted(a_dictionary):
+        print(f"{key}: {a_dictionary[key]}")
