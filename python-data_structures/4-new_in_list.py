7875f42142a7d16849c3bb7c8d47fa3afac3f478
eminbaxishli514-cloud <eminbaxishli514@gmail.com>
2025-11-19 09:49:13 +0000

    My new message

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def new_in_list((my_list, idx, element):
+def new_in_list(my_list, idx, element):
     """Replace an element at a given index in a list."""
     if 0 <= idx < len(my_list):
         my_list[idx] = element
