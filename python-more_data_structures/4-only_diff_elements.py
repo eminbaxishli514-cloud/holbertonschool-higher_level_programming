f06f853707ab1ea63752a10b1e7beb914df6f77d
eminbaxishli514-cloud <eminbaxishli514@gmail.com>
2025-11-19 12:47:04 +0000

    My new commit

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-	new_set = set1 | set2
-	return new_set
+    """Return a set of elements present in only one of the two sets."""
+    return set_1 ^ set_2
