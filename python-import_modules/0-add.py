af7fb64967e4fe345d5817970d3bd5e71074514a
eminbaxishli514-cloud <eminbaxishli514@gmail.com>
2025-11-19 08:16:17 +0000

    My new commit

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
 
-# Import the add function from add_0.py (only one mention of add_0)
 from add_0 import add
 
-# Assign values to a and b in separate lines
-a = 1
-b = 2
-
-# Print the sum in the requested format
-print("{} + {} = {}".format(a, b, add(a, b)))
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
