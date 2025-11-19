13d3950f7bf337b0a33eee99bb0c3a7bbf4f8cd5
eminbaxishli514-cloud <eminbaxishli514@gmail.com>
2025-11-19 10:33:29 +0000

    My new commit

---
+++ python-data_structures/11-delete_at.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    """
+    Delete the item at a specific position in a list.
+    If idx is out of range or negative, returns the list unchanged.
+    """
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+
+    new_list = []
+    for i in range(len(my_list)):
+        if i != idx:
+            new_list.append(my_list[i])
+    return new_list
