acb48455ef934cb8f4e297377ad467026d07965b
eminbaxishli514-cloud <eminbaxishli514@gmail.com>
2025-11-19 08:23:21 +0000

    My new commit

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,26 +1,12 @@
 #!/usr/bin/python3
+# Import only the needed functions once
+from calculator_1 import add, sub, mul, div
 
 a = 10
 b = 5
 
-def add(x, y):
-    """Return the sum of x and y"""
-    return x + y
-
-def sub(x, y):
-    """Return the difference of x and y"""
-    return x - y
-
-def mul(x, y):
-    """Return the product of x and y"""
-    return x * y
-
-def div(x, y):
-    """Return the integer division of x by y"""
-    return x // y  # integer division
-
 if __name__ == "__main__":
-    print("Addition: {} + {} = {}".format(a, b, add(a, b)))
-    print("Subtraction: {} - {} = {}".format(a, b, sub(a, b)))
-    print("Multiplication: {} * {} = {}".format(a, b, mul(a, b)))
-    print("Division: {} / {} = {}".format(a, b, div(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
