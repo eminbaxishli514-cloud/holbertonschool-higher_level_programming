76e4f905dcb2352544378c4b56fd0c941b942157
eminbaxishli514-cloud <eminbaxishli2@gmail.com>
2025-11-26 11:10:19 +0400

    My new commit

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
+
+
 def lenim(x):
-        k = 0
-        for i in x:
-                k += 1
-        return k
+    k = 0
+    for _ in x:
+        k += 1
+    return k
+
 
 def safe_print_list(my_list=[], x=0):
-        count = 0
-        try:
-                for i in range(0, x):
-                        print(my_list[i], end="")
-                        count += 1
-        except:
-                pass
-        print()
-        return count
+    count = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+            count += 1
+    except IndexError:
+        pass
+    print()
+    return count
