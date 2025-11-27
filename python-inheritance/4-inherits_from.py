dfcb75305243330035df2394660061b9176bc818
eminbaxishli514-cloud <eminbaxishli514@gmail.com>
2025-11-27 07:26:02 -0500

    Next task

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,4 +4,6 @@
 
 def inherits_from(obj, a_class):
     """Return True if obj is an instance of a class that inherited from a_class."""
-    return isinstance(obj, a_class) and type(obj) is not a_class
+    return (
+        isinstance(obj, a_class) and type(obj) is not a_class
+    )
