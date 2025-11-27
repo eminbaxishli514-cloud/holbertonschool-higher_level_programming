f6e94c7650129d19d21ee0e8af51d6b45b300287
eminbaxishli514-cloud <eminbaxishli514@gmail.com>
2025-11-27 04:43:00 -0500

    new

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,28 +1,10 @@
 #!/usr/bin/python3
-"""Module that defines the MyList class with custom sorting."""
+"""Module that defines the MyList class."""
 
 
 class MyList(list):
-    """A class that inherits from list and prints a sorted version."""
-
-    def _custom_sort(self, items):
-        """Sort items using a manual recursive algorithm."""
-        if len(items) < 2:
-            return items
-
-        pivot = items[0]
-        left = []
-        right = []
-
-        for value in items[1:]:
-            if value < pivot:
-                left.append(value)
-            else:
-                right.append(value)
-
-        return self._custom_sort(left) + [pivot] + self._custom_sort(right)
+    """Class that inherits from list."""
 
     def print_sorted(self):
-        """Print the list sorted in ascending order."""
-        sorted_list = self._custom_sort(self[:])
-        print(sorted_list)
+        """Prints the list in ascending order."""
+        print(sorted(self))
