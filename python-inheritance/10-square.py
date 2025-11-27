8e4d3b2b01ef7e1cd3bc17e9d505094b24be25ed
eminbaxishli514-cloud <eminbaxishli514@gmail.com>
2025-11-27 08:33:31 -0500

    My new commit

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Square class that inherits from Rectangle."""
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """Represent a square."""
+
+    def __init__(self, size):
+        """Initialize a square with a private size."""
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """Return the area of the square."""
+        return self.__size ** 2
