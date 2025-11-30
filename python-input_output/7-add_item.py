e6ad82847f121c8f08848865f2f7b164ddcb5e1d
eminbaxishli514-cloud <eminbaxishli514@gmail.com>
2025-11-30 05:44:07 -0500

    Update

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
-Script that adds command line arguments to a Python list
+Script that adds command-line arguments to a Python list
 and saves them to a JSON file.
 """
 
 import sys
 import json
-from 5-save_to_json_file import save_to_json_file
-from 6-load_from_json_file import load_from_json_file
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
 
 filename = "add_item.json"
 
