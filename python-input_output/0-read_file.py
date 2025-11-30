5d326434d314155c0944b04e3b232642fdda8950
eminbaxishli514-cloud <eminbaxishli514@gmail.com>
2025-11-30 05:16:31 -0500

    My new commit

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+"""
+This module provides a function to read a UTF-8 text file
+and print its contents to stdout.
+"""
+
+
 def read_file(filename=""):
-    """Reads a text file (UTF8) and prints it to stdout"""
+    """
+    Reads a text file (UTF-8) and prints its content to stdout.
+
+    Args:
+        filename (str): The path to the text file.
+    """
     with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
