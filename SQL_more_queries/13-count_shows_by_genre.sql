7508132b054cd00487ea811196d705695150fecf
eminbaxishli514-cloud <eminbaxishli514@gmail.com>
2025-12-03 17:41:32 +0400

    Update

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,7 @@
 -- 13-count_shows_by_genre.sql
 SELECT g.name AS genre, COUNT(tg.show_id) AS number_of_shows
-FROM tv_show_genres tg
-JOIN genres g ON tg.genre_id = g.id
-GROUP BY g.name
+FROM tv_genres g
+JOIN tv_show_genres tg ON g.id = tg.genre_id
+GROUP BY g.id
 ORDER BY number_of_shows DESC;
+
