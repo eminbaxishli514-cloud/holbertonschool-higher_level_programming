349d3401c4d91b2dc8f3889764afe6fed21c92d7
eminbaxishli514-cloud <eminbaxishli514@gmail.com>
2025-12-03 17:13:24 +0400

    Update

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,7 +1,6 @@
 #!/usr/bin/sql
--- Script that creates the MySQL user user_0d_1 with all privileges
--- If the user already exists, the script must not fail
+-- Create user user_0d_1 with all privileges
 
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
