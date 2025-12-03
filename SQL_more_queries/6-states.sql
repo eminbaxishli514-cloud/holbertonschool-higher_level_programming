42aeae85c1e7a135c41bc8c1e8ad089097e233f4
eminbaxishli514-cloud <eminbaxishli514@gmail.com>
2025-12-03 17:22:20 +0400

    Update

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,11 @@
+#!/usr/bin/sql
+-- Create database and states table
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS states (
+    id INT NOT NULL AUTO_INCREMENT UNIQUE PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);
