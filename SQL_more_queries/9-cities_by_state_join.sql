a0993d9480705f25760c89dba1085bb561c73ea0
eminbaxishli514-cloud <eminbaxishli514@gmail.com>
2025-12-03 17:25:44 +0400

    Update

---
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -0,0 +1,7 @@
+#!/usr/bin/sql
+-- List all cities with their state names
+
+SELECT cities.id, cities.name, states.name
+FROM cities
+JOIN states ON cities.state_id = states.id
+ORDER BY cities.id ASC;
