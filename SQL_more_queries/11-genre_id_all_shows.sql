04489aa84d9eb061766be0b71fa7499c97141cb8
eminbaxishli514-cloud <eminbaxishli514@gmail.com>
2025-12-03 17:28:58 +0400

    Update

---
+++ SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,7 @@
+-- 11-genre_id_all_shows.sql
+-- List all shows, showing genre_id if it exists, NULL otherwise
+
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
