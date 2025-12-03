6b45c77539050ce745ad043baf2c81acb2c5c97e
eminbaxishli514-cloud <eminbaxishli514@gmail.com>
2025-12-03 17:38:46 +0400

    Update

---
+++ SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,7 @@
+-- 15-comedy_only.sql
+SELECT t.title
+FROM tv_shows t
+JOIN tv_show_genres tg ON t.id = tg.show_id
+JOIN tv_genres g ON tg.genre_id = g.id
+WHERE g.name = 'Comedy'
+ORDER BY t.title ASC;
