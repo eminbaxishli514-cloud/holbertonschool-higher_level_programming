4dace813aae41ace971ddabaeec46e38d760707b
eminbaxishli514-cloud <eminbaxishli514@gmail.com>
2025-12-03 15:06:24 +0400

    Update

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -17,7 +17,10 @@ if __name__ == "__main__":
         response = requests.post(url, data=data)
         response_json = response.json()
         if response_json:
-            print("[{}] {}".format(response_json.get("id"), response_json.get("name")))
+            print("[{}] {}".format(
+                response_json.get("id"),
+                response_json.get("name")
+            ))
         else:
             print("No result")
     except ValueError:
