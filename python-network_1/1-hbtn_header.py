2cdfc02dbab105ec6f0151cb83151e09e8630b0b
eminbaxishli514-cloud <eminbaxishli514@gmail.com>
2025-12-03 14:13:36 +0400

    Update

---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Sends a request to a URL and displays the value
+of the X-Request-Id header from the response.
+"""
+
+import urllib.request
+import sys
+
+url = sys.argv[1]
+
+with urllib.request.urlopen(url) as response:
+    headers = response.headers
+    print(headers.get("X-Request-Id"))
